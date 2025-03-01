7bf95375c1ddcae94102754f093bb2e042dfa248
이경용 <sleekydz86@naver.com>
2025-03-01 16:10:48 +0900

    feat:프로젝트 셋팅

---
+++ Backend/src/main/kotlin/com/ticket/ticketnoti/TicketNotiApplication.kt
@@ -0,0 +1,11 @@
+package com.ticket.ticketnoti
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketNotiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TicketNotiApplication>(*args)
+}
---
+++ Backend/src/test/kotlin/com/ticket/ticketnoti/TicketNotiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ticket.ticketnoti
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicketNotiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
